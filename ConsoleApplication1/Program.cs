f9920066cb0d4129420a2274310a764df7b40955
Teodor Kostadinov <teodor.kk.93@gmail.com>
2013-04-14 00:05:16 +0300

    1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 0) throw new FormatException();
+
+            Console.WriteLine(Math.Sqrt(n));
+        }
+
+        catch (ArgumentNullException)
+        {
+            Console.Error.WriteLine("Invalid number");
+        }
+
+        catch (FormatException)
+        {
+            Console.Error.WriteLine("Invalid number");
+        }
+
+        catch (OverflowException)
+        {
+            Console.Error.WriteLine("Invalid number");
+        }
+
+        finally
+        {
+            Console.WriteLine("Good bye!");
+        }
+    }
+}
